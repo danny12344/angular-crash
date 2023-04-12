db623c7e6bb4893f5cabd4df7032e7585d9332fd
Dan Simons <danielsimons370@gmail.com>
2023-04-12 12:18:59 +0100

    generating boiler plate for tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 // when creating components using cli tool they are imported here automatically
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
