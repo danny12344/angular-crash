9dd54044af7fa013d3f12d01ecce562717f478fa
Dan Simons <danielsimons370@gmail.com>
2023-04-12 10:04:14 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
+// see html file - the selector "app-root" is loaded in the index.html in the body sectoin
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// this where you put any properties of your component e.g. title, methods
 export class AppComponent {
-  title = 'angular-crash';
+  title: string = 'Task Tracker';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// root component
 import { AppComponent } from './app.component';
 
+// when creating components using cli tool they are imported here automatically
 @NgModule({
   declarations: [
     AppComponent
